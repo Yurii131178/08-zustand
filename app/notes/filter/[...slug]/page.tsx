282374a74e042b7dadbd7d06ecd90e0fb96f9ef2
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-05 16:00:59 +0300

    fix issues with meta tags

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,26 +1,23 @@
+import { Metadata } from 'next';
+import { Tag } from '@/types/note';
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
-import { Tag } from '@/types/note';
-import type { Metadata } from 'next';
 
 interface NotesProps {
   params: Promise<{ slug: string[] }>;
 }
 
-// Функція для генерації метаданих сторінки на основі фільтру
 export async function generateMetadata({
   params,
-}: {
-  params: { slug: string[] };
-}): Promise<Metadata> {
-  const { slug } = params;
+}: NotesProps): Promise<Metadata> {
+  const { slug } = await params; // Await the Promise to get the slug
   const tag = slug.length > 0 && slug[0] !== 'All' ? (slug[0] as Tag) : 'All';
 
-  const title = tag === 'All' ? 'All Notes' : `${tag} Notes`;
+  const title = tag === 'All' ? 'All Notes' : `Notes tagged "${tag}"`;
   const description =
     tag === 'All'
-      ? 'View all available notes sorted by date created.'
-      : `View notes filtered by tag «${tag}».`;
+      ? 'View all notes sorted by creation date.'
+      : `Explore notes filtered by the "${tag}" tag.`;
 
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
   const url = `${baseUrl}/notes/filter/${slug.join('/') || 'All'}`;
@@ -45,7 +42,6 @@ export async function generateMetadata({
   };
 }
 
-// Сторінка з нотатками, фільтрованими за тегом
 const Notes = async ({ params }: NotesProps) => {
   const { slug } = await params;
   const tag =
