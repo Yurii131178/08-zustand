e9515334e48a46e28dae5d5fb80ae38a6b41dc22
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-05 12:06:41 +0300

    add meta tags to app/layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,26 @@ import Header from '@/components/Header/Header';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
 export const metadata: Metadata = {
-  title: 'Notehub App',
-  description: 'Your personal note-taking application',
+  title: 'NoteHub',
+  description:
+    'NoteHub - your assistant for note management. Create, sort by categories, and boost productivity with our intuitive app!',
+  keywords:
+    'notes, NoteHub, note manager, organization, personal organizer, daily planner',
+  openGraph: {
+    title: 'NoteHub - Your Ultimate Note-Taking Assistant',
+    description:
+      'Manage your notes effortlessly with NoteHub. Create, organize by categories, and enhance productivity',
+    url: '', // після деплою додам сюди посилання типу 'https://yurii131178.github.io/0000000000000000000/'
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        alt: 'NoteHub - Note Management App Logo',
+        width: '1200',
+        height: '630',
+      },
+    ],
+    type: 'article',
+  },
 };
 
 interface RootLayoutProps {
