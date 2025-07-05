b2e5ed79a58bde668480285aa82b4ffaf39084d0
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-05 13:49:06 +0300

    add meta tags to not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,25 @@
+import { Metadata } from 'next';
 import css from './not-found.module.css';
+
+export const metadata: Metadata = {
+  title: '404 - Page not found',
+  description: 'Sorry, the page you are looking for does not exist',
+  openGraph: {
+    title: '404 - Page not found',
+    description: 'Sorry, the page you are looking for does not exist',
+    url: 'https://notehub.com/404',
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        alt: 'page NoteHub not found | 404',
+        width: '1200',
+        height: '630',
+      },
+    ],
+    type: 'website',
+  },
+};
 
 const NotFound = () => {
   return (
