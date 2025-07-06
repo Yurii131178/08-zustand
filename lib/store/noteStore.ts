2877ef7c790ad86c2f1750a322803c345297fa0b
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-06 10:49:15 +0300

    install Zustand_implement a Z-page

---
+++ lib/store/noteStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+import { NewNoteData } from '@/types/note';
+
+interface NoteStore {
+  draft: NewNoteData;
+  setDraft: (note: Partial<NewNoteData>) => void;
+  clearDraft: () => void;
+}
+
+const initialDraft: NewNoteData = {
+  title: '',
+  content: '',
+  tag: 'Todo',
+};
+
+export const useNoteStore = create<NoteStore>()((set) => ({
+  draft: initialDraft,
+
+  setDraft: (note) =>
+    set((state) => ({
+      draft: { ...state.draft, ...note },
+    })),
+  clearDraft: () => set({ draft: initialDraft }),
+}));
