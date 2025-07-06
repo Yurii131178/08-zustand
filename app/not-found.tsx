e28094dbc2573cb37c2f913f0859f605122f2efd
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-06 23:02:51 +0300

    fix the issue with the correct url

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,17 +2,17 @@ import { Metadata } from 'next';
 import css from './not-found.module.css';
 
 export const metadata: Metadata = {
-  title: '404 - Page not found',
+  title: '404 - Page Not Found | NoteHub',
   description: 'Sorry, the page you are looking for does not exist',
   openGraph: {
-    title: '404 - Page not found',
+    title: '404 - Page Not Found | NoteHub',
     description: 'Sorry, the page you are looking for does not exist',
-    url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+    url: 'https://notehub.com/404',
     siteName: 'NoteHub',
     images: [
       {
         url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-        alt: 'page NoteHub not found | 404',
+        alt: 'NoteHub 404',
         width: 1200,
         height: 630,
       },
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 const NotFound = () => {
   return (
     <div>
-      <h1 className={css.title}>404 - Page not found</h1>
+      <h1 className={css.title}>404 - Page Not Found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
